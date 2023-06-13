3260a8588476ef9b5337211e2793f0a52d7d9d81
alessandra Marques <alessandra2309@gmail.com>
2023-06-13 14:51:02 -0400

    prova

---
+++ provaoficial/correcao.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace provaoficial
+{
+    public partial class correcao : Form
+    {
+        public correcao()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gabarito c = new gabarito();
+            c.ShowDialog();
+            //correcao 
+            string correcao = "a" ;
+            string correcao1 = "b";
+            string correcao2 = "c";
+            string correcao3 = "d";
+            string correcao4 = "e";
+          
+           
+
+            for (int i  = 0; i < 10; i++)
+            {
+               
+                if(i == 0)
+                {
+                    Console.WriteLine($"seu resultado é {correcao}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 1)
+                {
+                    Console.WriteLine($"seu resultado é {correcao1}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 2)
+                {
+                    Console.WriteLine($"seu resultado é {correcao2}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 3)
+                {
+                    Console.WriteLine($"seu resultado é {correcao3}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 4)
+                {
+                    Console.WriteLine($"seu resultado é {correcao3}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 5)
+                {
+                    Console.WriteLine($"seu resultado é {correcao4}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 6)
+                {
+                    Console.WriteLine($"seu resultado é {correcao}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 7)
+                {
+                    Console.WriteLine($"seu resultado é {correcao2}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 8)
+                {
+                    Console.WriteLine($"seu resultado é {correcao1}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 9)
+                {
+                    Console.WriteLine($"seu resultado é {correcao4}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+
+                if (i == 10)
+                {
+                    Console.WriteLine($"seu resultado é {correcao}");
+                }
+                else
+                {
+                    Console.WriteLine($"seu resultado esta incorreto");
+                }
+            }
+
+           /*  string[] corrigir = { "a", "b", "c", "d" };
+
+            if (corrigir.Length > 0)
+            {
+                corrigir[1] = "a";
+            }*/
+
+
+        }
+    }
+}
---
+++ provaoficial/gabarito.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace provaoficial
+{
+    partial class gabarito
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.bt_comparar = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(143, 123);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(143, 164);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(143, 239);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // bt_comparar
+            // 
+            this.bt_comparar.Location = new System.Drawing.Point(209, 292);
+            this.bt_comparar.Name = "bt_comparar";
+            this.bt_comparar.Size = new System.Drawing.Size(75, 23);
+            this.bt_comparar.TabIndex = 4;
+            this.bt_comparar.Text = "comparar";
+            this.bt_comparar.UseVisualStyleBackColor = true;
+            this.bt_comparar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(280, 239);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(143, 204);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(280, 81);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(280, 123);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(280, 164);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(280, 204);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 10;
+            // 
+            // gabarito
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(538, 450);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.bt_comparar);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "gabarito";
+            this.Text = "gabarito";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button bt_comparar;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+    }
+}
